2e416674239e1e2d766fee6364b45ccecb729760
leeyang2019 <nyang@madisoncollege.edu>
2020-12-04 12:10:23 -0500

    created shipping and payment routes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -10,6 +10,8 @@ import LoginScreen from './screens/LoginScreen';
 import RegisterScreen from './screens/RegisterScreen';
 import ProfileScreen from './screens/ProfileScreen';
 import UserListScreen from './screens/ListScreen';
+import ShippingScreen from './screens/ShippingScreen';
+import PaymentScreen from './screens/PaymentMethodScreen';
 
 const App = () => {
 	return (
@@ -18,6 +20,8 @@ const App = () => {
 			<main className="py-3">
 				<Container>
 					<Route path="/login" component={LoginScreen} exact />
+					<Route path="/shipping" component={ShippingScreen} exact />
+					<Route path="/payment" component={PaymentScreen} exact />
 					<Route path="/register" component={RegisterScreen} exact />
 					<Route path="/profile" component={ProfileScreen} exact />
 					<Route path="/products/:id" component={ProductScreen} exact />
