562a8a2e510a0d919b1a2d018ae07f4ec059a970
leeyang2019 <nyang@madisoncollege.edu>
2020-12-19 21:39:25 -0500

    updated ProfileScreen

--- frontend/src/screens/ProfileScreen copy.js
+++ frontend/src/screens/ProfileScreen copy.js
@@ -1,14 +1,14 @@
 import React, { useState, useEffect } from 'react';
-import { Table, Form, Button, Row, Col } from 'react-bootstrap';
-import { LinkContainer } from 'react-router-bootstrap';
+import { Form, Button, Row, Col, Table } from 'react-bootstrap';
 import { useDispatch, useSelector } from 'react-redux';
-import Message from '../components/Message';
-import Loader from '../components/Loader';
+import Message from '../components/Message.js';
+import Loader from '../components/Loader.js';
 import { getUserDetails, updateUserProfile } from '../actions/userActions';
 import { listMyOrders } from '../actions/orderActions';
-import { USER_UPDATE_PROFILE_RESET } from '../constants/userContants';
+import { LinkContainer } from 'react-router-bootstrap';
 
 const ProfileScreen = ({ location, history }) => {
+	//define states
 	const [name, setName] = useState('');
 	const [email, setEmail] = useState('');
 	const [password, setPassword] = useState('');
@@ -17,9 +17,11 @@ const ProfileScreen = ({ location, history }) => {
 
 	const dispatch = useDispatch();
 
+	//call the userDetails reducer in the store
 	const userDetails = useSelector((state) => state.userDetails);
 	const { loading, error, user } = userDetails;
 
+	//call the userLogin reducer in the store
 	const userLogin = useSelector((state) => state.userLogin);
 	const { userInfo } = userLogin;
 
@@ -33,22 +35,26 @@ const ProfileScreen = ({ location, history }) => {
 		if (!userInfo) {
 			history.push('/login');
 		} else {
-			if (!user || !user.name || success) {
-				dispatch({ type: USER_UPDATE_PROFILE_RESET });
+			//if user.name does not exist, fetch data
+			if (!user.name) {
 				dispatch(getUserDetails('profile'));
 				dispatch(listMyOrders());
 			} else {
+				//after fetching, set name & email
 				setName(user.name);
 				setEmail(user.email);
 			}
 		}
-	}, [dispatch, history, userInfo, user, success]);
+	}, [dispatch, history, userInfo, user]);
 
 	const submitHandler = (e) => {
 		e.preventDefault();
+		//check password against confirmPassword
 		if (password !== confirmPassword) {
+			//set new message if not matched
 			setMessage('Passwords do not match');
 		} else {
+			//otherwise, dispatch
 			dispatch(updateUserProfile({ id: user._id, name, email, password }));
 		}
 	};
@@ -58,75 +64,66 @@ const ProfileScreen = ({ location, history }) => {
 			<Col md={3}>
 				<h2>User Profile</h2>
 				{message && <Message variant="danger">{message}</Message>}
-				{}
+				{error && <Message variant="danger">{error}</Message>}
 				{success && <Message variant="success">Profile Updated</Message>}
-				{loading ? (
-					<Loader />
-				) : error ? (
-					<Message variant="danger">{error}</Message>
-				) : (
-					<Form onSubmit={submitHandler}>
-						<Form.Group controlId="name">
-							<Form.Label>Name</Form.Label>
-							<Form.Control
-								type="name"
-								placeholder="Enter name"
-								value={name}
-								onChange={(e) => setName(e.target.value)}
-							></Form.Control>
-						</Form.Group>
-
-						<Form.Group controlId="email">
-							<Form.Label>Email Address</Form.Label>
-							<Form.Control
-								type="email"
-								placeholder="Enter email"
-								value={email}
-								onChange={(e) => setEmail(e.target.value)}
-							></Form.Control>
-						</Form.Group>
-
-						<Form.Group controlId="password">
-							<Form.Label>Password</Form.Label>
-							<Form.Control
-								type="password"
-								placeholder="Enter password"
-								value={password}
-								onChange={(e) => setPassword(e.target.value)}
-							></Form.Control>
-						</Form.Group>
-
-						<Form.Group controlId="confirmPassword">
-							<Form.Label>Confirm Password</Form.Label>
-							<Form.Control
-								type="password"
-								placeholder="Confirm password"
-								value={confirmPassword}
-								onChange={(e) => setConfirmPassword(e.target.value)}
-							></Form.Control>
-						</Form.Group>
-
-						<Button type="submit" variant="primary">
-							Update
-						</Button>
-					</Form>
-				)}
+				{loading && <Loader />}
+				<Form onSubmit={submitHandler}>
+					<Form.Group controlId="name">
+						<Form.Label>Name</Form.Label>
+						<Form.Control
+							type="name"
+							placeholder="Enter name"
+							value={name}
+							onChange={(e) => setName(e.target.value)}
+						></Form.Control>
+					</Form.Group>
+					<Form.Group controlId="email">
+						<Form.Label>Email Address</Form.Label>
+						<Form.Control
+							type="email"
+							placeholder="Enter email"
+							value={email}
+							onChange={(e) => setEmail(e.target.value)}
+						></Form.Control>
+					</Form.Group>
+					<Form.Group controlId="password">
+						<Form.Label>Password</Form.Label>
+						<Form.Control
+							type="password"
+							placeholder="Enter password"
+							value={password}
+							onChange={(e) => setPassword(e.target.value)}
+						></Form.Control>
+					</Form.Group>
+					<Form.Group controlId="confirmPassword">
+						<Form.Label>Confirm Password</Form.Label>
+						<Form.Control
+							type="password"
+							placeholder="Confirm password"
+							value={confirmPassword}
+							onChange={(e) => setConfirmPassword(e.target.value)}
+						></Form.Control>
+					</Form.Group>
+					<Button type="submit" variant="primary">
+						Update
+					</Button>
+				</Form>
 			</Col>
 			<Col md={9}>
 				<h2>My Orders</h2>
 				{loadingOrders ? (
 					<Loader />
-				) : errorOrders ? (
+				) : error ? (
 					<Message variant="danger">{errorOrders}</Message>
 				) : (
 					<Table striped bordered hover responsive className="table-sm">
 						<thead>
 							<tr>
 								<th>ID</th>
-								<th>DATE</th>
-								<th>TOTAL</th>
-								<th>PAID</th>
-								<th>DELIVERED</th>
+								<th>Date</th>
+								<th>Total</th>
+								<th>Paid</th>
+								<th>Delivered</th>
 								<th></th>
 							</tr>
 						</thead>
